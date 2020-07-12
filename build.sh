e3028e95a27e8b12d7c4e94f484bb0db2db74e36
Anderson Mendes <anderson2000ano@gmail.com>
2020-07-12 09:47:11 -0300

    build: update gcc path

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 make O=out ARCH=arm64 mido_defconfig
 
-PATH="$HOME/android/aosp10/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:${PATH}" \
+PATH="$HOME/mido/aarch64-linux-android-4.9/aarch64-linux-android/bin:${PATH}" \
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
                       CROSS_COMPILE=aarch64-linux-android-
