3f283b20c04bcdfaf333f0af3ff5494d5e7c649a
Anderson Mendes <anderson2000ano@gmail.com>
2020-07-12 10:41:28 -0300

    fixup build

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 make O=out ARCH=arm64 mido_defconfig
 
-PATH="$HOME/mido/aarch64-linux-android-4.9/aarch64-linux-android/bin:${PATH}" \
+PATH="$HOME/mido/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9/bin:${PATH}" \
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
                       CROSS_COMPILE=aarch64-linux-android-
+
